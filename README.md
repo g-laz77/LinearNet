0176a8288cebbb089226b247e3682d7181c0d08b
Bakhtiyar Syed <syed.b@research.iiit.ac.in>
2017-10-26 01:36:42 +0530

    Transferred models for now

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Word-Embedding-Projection-As-Linear-Transformation
+Training a transformation matrix which can be used to “connect” word of language L1 in embedding space S1 to word of language L2 in embedding space S2.
+
+Models and datasets are hosted at 10.4.17.65
---
+++ src/train.py
@@ -0,0 +1,24 @@
+import sys, gensim, logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+#Supply as command line args
+fname = sys.argv[1]
+
+
+#If input is in a large file , use this!
+
+class LoadFile(object):
+    #Returns an iterator to iterate on...
+    def __init__(self, filename):
+        self.filename = filename
+    def __iter__(self):
+        for line in open('./'+self.filename):
+            yield line.split()
+
+
+sentences = LoadFile(sys.argv[1])
+model = gensim.models.Word2Vec(sentences, min_count = 4, size = 300)
+
+model.save('./models/en')
+
+
